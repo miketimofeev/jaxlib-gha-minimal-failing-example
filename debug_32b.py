e7eb5f66634435caece27fcf115830215afa710f
Matthew Feickert <matthew.feickert@cern.ch>
2021-06-30 19:52:17 -0500

    Add more prints as requested

--- debug_32b.py
+++ debug_32b.py
@@ -1,5 +1,9 @@
 import jax  # noqa: F401
+
+print(jax)
 import jax.numpy as jnp
+
+print(jnp)
 
 print(jnp.asarray([-2, -1], dtype=jnp.float32))
 print(jnp.asarray([-2, -1], dtype=jnp.float64))
--- debug_64b.py
+++ debug_64b.py
@@ -1,10 +1,17 @@
 import jax  # noqa: F401
+
+print(jax)
 from jax.config import config
 
+print(config)
+
 config.update("jax_enable_x64", True)
+print(config)
 import jax.numpy as jnp
 
+print(jnp)
+
 # 32b first
-jnp.asarray([-2, -1])
+print(jnp.asarray([-2, -1]))
 # then switch to 64b
-jnp.asarray([-2, -1], dtype=jnp.float64)
+print(jnp.asarray([-2, -1], dtype=jnp.float64))

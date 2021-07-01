6070cc90c94d9ba990605e03251b4a523e1f10c3
Matthew Feickert <matthew.feickert@cern.ch>
2021-06-30 20:29:38 -0500

    Add prints to the whole file

--- debug_32b.py
+++ debug_32b.py
@@ -7,7 +7,12 @@ print(jax)
 print("Ran: print(jax)")
 import jax.numpy as jnp
 
+print("Ran: import jax.numpy as jnp")
+
 print(jnp)
+print("Ran: print(jnp)")
 
 print(jnp.asarray([-2, -1], dtype=jnp.float32))
+print("Ran: print(jnp.asarray([-2, -1], dtype=jnp.float32))")
 print(jnp.asarray([-2, -1], dtype=jnp.float64))
+print("Ran: print(jnp.asarray([-2, -1], dtype=jnp.float64))")
